5ea965238fcc6c53cf59b821615c4ab6cb6df4b9
leibaustria <161556597+leibaust@users.noreply.github.com>
2024-10-08 15:11:00 -0700

    code cleanup

--- script/script.js
+++ script/script.js
@@ -1,34 +1,33 @@
-
 //Aquired via ChatGPT to change project images on hover
 
-document.addEventListener('DOMContentLoaded', () => {
-  const projectLinks = document.querySelectorAll('.project-link');
+document.addEventListener("DOMContentLoaded", () => {
+  const projectLinks = document.querySelectorAll(".project-link");
 
   projectLinks.forEach((link, index) => {
-    const img = link.querySelector('img');
+    const img = link.querySelector("img");
     const originalSrc = img.src;
-    let hoverSrc = '';
+    let hoverSrc = "";
 
     switch (index) {
       case 0:
-        hoverSrc = 'images/html-dark.svg';
+        hoverSrc = "images/html-dark.svg";
         break;
       case 1:
-        hoverSrc = 'images/java-dark.svg';
+        hoverSrc = "images/java-dark.svg";
         break;
       case 2:
-        hoverSrc = 'images/wordpress-dark.svg';
+        hoverSrc = "images/wordpress-dark.svg";
         break;
       case 3:
-        hoverSrc = 'images/stores-dark.svg';
+        hoverSrc = "images/stores-dark.svg";
         break;
     }
 
-    link.addEventListener('mouseover', () => {
+    link.addEventListener("mouseover", () => {
       img.src = hoverSrc;
     });
 
-    link.addEventListener('mouseout', () => {
+    link.addEventListener("mouseout", () => {
       img.src = originalSrc;
     });
   });
