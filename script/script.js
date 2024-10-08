d73f449924714285269f337793133d6420eeb895
leibaustria <161556597+leibaust@users.noreply.github.com>
2024-10-08 14:28:17 -0700

    added javascript to change images on hover to dark versions - ref: chatgpt

--- script/script.js
+++ script/script.js
@@ -1,6 +1,35 @@
-// Ref: https://codepen.io/mblode/pen/YzozOm
 
-$('.menu').click (function(){
-    $(this).toggleClass('open');
+//Aquired via ChatGPT to change project images on hover
+
+document.addEventListener('DOMContentLoaded', () => {
+  const projectLinks = document.querySelectorAll('.project-link');
+
+  projectLinks.forEach((link, index) => {
+    const img = link.querySelector('img');
+    const originalSrc = img.src;
+    let hoverSrc = '';
+
+    switch (index) {
+      case 0:
+        hoverSrc = 'images/html-dark.svg';
+        break;
+      case 1:
+        hoverSrc = 'images/java-dark.svg';
+        break;
+      case 2:
+        hoverSrc = 'images/wordpress-dark.svg';
+        break;
+      case 3:
+        hoverSrc = 'images/stores-dark.svg';
+        break;
+    }
+
+    link.addEventListener('mouseover', () => {
+      img.src = hoverSrc;
+    });
+
+    link.addEventListener('mouseout', () => {
+      img.src = originalSrc;
+    });
   });
-  
+});
